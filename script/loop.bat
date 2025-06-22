365ed4c4ff768a17c9cfcaa07d0445fd33564824
kruz <121413821+khrsu@users.noreply.github.com>
2025-06-22 20:52:53 +0700

    Update loop.bat

--- script/loop.bat
+++ script/loop.bat
@@ -1,10 +1,3 @@
-@echo off
-echo =================================================================
-echo  Starting infinite loop to keep the runner alive.
-echo  This session will be terminated automatically after 6 hours.
-echo  You can manually stop this from the GitHub Actions page.
-echo =================================================================
-
 :loop
 echo [%TIME%] Runner is alive...
 timeout /t 60 /nobreak >nul
